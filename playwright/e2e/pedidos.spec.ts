c9780d64c34df903465fec9830ca571e9b12979a
Eduardo <maguines_01@yahoo.com.br>
2026-02-08 09:05:14 -0300

    meu primeiro commit com o git

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test'
+
+/// AAA - Arrange, Act, Assert
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Test Data
+  const order = 'VLO-1S82UG'
+
+  // Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  // Act  
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  // Assert
+
+  const containerPedido = page.getByRole('paragraph')
+    .filter({ hasText: /^Pedido$/ })
+    .locator('..') //Sobe para o elemento pai (a div que agrupa ambos)
+
+  await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+
+  await expect(page.getByText('APROVADO')).toBeVisible()
+
+})
