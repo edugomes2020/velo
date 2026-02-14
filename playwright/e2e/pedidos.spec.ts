725ca938dcfcd26439f4340e9dc44ede52c9388d
Eduardo <maguines_01@yahoo.com.br>
2026-02-14 20:16:27 -0300

    Criando o segundo teste e o arquivo helpers de geração de pedido

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+import { generateOrderCode } from '../support/helpers'
+
 /// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
@@ -26,4 +28,24 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(containerPedido).toContainText('VLO-1S82UG')
 
     await expect(page.getByText('APROVADO')).toBeVisible()
+})
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+
+  const order = generateOrderCode()
+
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+  `)
 })
